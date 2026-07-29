f99c043a92d220d1f83c7e388d1f7c81a7ceffcc
Yazel Hipolito <yazelhipolito@gmail.com>
2026-07-29 04:43:28 -0600

    added oath funtionality

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const passport = require('passport');
 const airportsController = require('../controllers/airports');
+const { isAuthenticated } = require("../middleware/authenticate");
 const helicoptersController = require('../controllers/helicopters');
 const validationGuard = require('../middleware/validate');
 
@@ -12,15 +14,26 @@ router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 // --- Airports API Endpoints ---
 router.get('/airports', airportsController.getAllAirports);
 router.get('/airports/:id', airportsController.getSingleAirport);
-router.post('/airports', validationGuard.saveAirport, airportsController.createAirport);
-router.put('/airports/:id', validationGuard.saveAirport, airportsController.updateAirport); // Added with validation
-router.delete('/airports/:id', airportsController.deleteAirport); // Added
+router.post('/airports', isAuthenticated, validationGuard.saveAirport, airportsController.createAirport);
+router.put('/airports/:id', isAuthenticated, validationGuard.saveAirport, airportsController.updateAirport); // Added with validation
+router.delete('/airports/:id', isAuthenticated, airportsController.deleteAirport); // Added
 
 // --- Helicopters API Endpoints ---
 router.get('/helicopters', helicoptersController.getAllHelicopters);
 router.get('/helicopters/:id', helicoptersController.getSingleHelicopter);
-router.post('/helicopters', validationGuard.saveHelicopter, helicoptersController.createHelicopter);
-router.put('/helicopters/:id', validationGuard.saveHelicopter, helicoptersController.updateHelicopter); // Added with validation
-router.delete('/helicopters/:id', helicoptersController.deleteHelicopter); // Added
+router.post('/helicopters', isAuthenticated, validationGuard.saveHelicopter, helicoptersController.createHelicopter);
+router.put('/helicopters/:id', isAuthenticated, validationGuard.saveHelicopter, helicoptersController.updateHelicopter); // Added with validation
+router.delete('/helicopters/:id', isAuthenticated, helicoptersController.deleteHelicopter); // Added
+
+router.get("/login", passport.authenticate("github"), (req, res) => {});
+
+router.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,24 +2,75 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const router = require('./routes/index');
+const session = require('express-session');
+const passport = require('passport');
+const cors = require('cors');
+const GitHubStrategy = require("passport-github2").Strategy;
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Ensure Content-Type, Accept, and Authorization are fully opened
-    res.setHeader(
-      'Access-Control-Allow-Headers', 
-      'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization'
+.use(bodyParser. json())
+.use(session({
+  secret: "secret",
+  resave: false,
+  saveUninitialized: true
+}))
+// This is the basic express session({..}) initialization.
+.use(passport.initialize())
+// init passport on every route call.
+.use(passport.session())
+// allow passport to use "express-session".
+.use((req, res, next) => {
+    res. setHeader("Acress-Controll-Allow-Origin", "*"); 
+    res .setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization"
+);
+res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, GET, PUT, PATCH, OPTIONS, DELETE"
     );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-  })
-  .use('/', router);
+next ();
+})
+.use(cors({methods: ["GET", "POST", "DELETE", "UPDATE", "PUT","PATCH" ]}))
+.use(cors ({ origin: "*" }))
+.use("/", require("./routes/index.js"));
 
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID, 
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+},
+function(accessToken, refreshtoken, profile, done) {
+    //User. findorCreate( githubId: profile.id }, function (err, user) {
+        return done (null, profile);
+    //});
+}
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get("/", (req, res) => {
+  res.send(
+    req.session.user !== undefined
+      ? `Logged in as ${req.session.user.displayName}`
+      : "Logged Out"
+  );
+});
+
+app.get('/github/callback', passport.authenticate('github', {
+  failureRedirect: '/api-docs', session: false}), 
+  (req, res) => {
+  req.session.user = req.user;
+  res.redirect('/');
+});
 
 mongodb.initDb((err) => {
   if (err) {
